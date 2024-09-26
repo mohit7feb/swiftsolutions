4d120d0f47a1bdba80bced4a46b434d604347f37
mohit7feb <45337317+mohit7feb@users.noreply.github.com>
2024-09-26 12:44:49 +0530

    Add files via upload

---
+++ info.php
@@ -0,0 +1,10 @@
+<?php
+	
+	$companyname = 'Swift Solutions';
+	
+    $address = '1234 Silicon Valley St, Suite 500, San Jose, CA 95110, USA';
+    $phone = '+1 408-123-4567';
+
+    $email = 'info@swiftsolution.com';
+
+	
---
+++ privacy-policy.php
@@ -0,0 +1,230 @@
+<?php include 'info.php' ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Privacy Policy</title>
+
+		<!--Import Google Icon Font-->
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<!--Import materialize.css-->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+		<link type="text/css" rel="stylesheet" href="css/website.css"  media="screen,projection"/>
+
+
+		<!--Let browser know website is optimized for mobile-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+	<style>
+            @media only screen and (max-width: 768px) {
+  /* CSS styles for mobile devices go here */
+  .flexbox{
+    display:block;
+  }
+  #industry_expertise{
+    margin-right:0px !important; 
+  }
+
+  .brand-logo{
+    font-size:22px !important;
+    margin-top:-9px !important;
+  }
+  .main-nav-res{
+    display:inline-grid !important;
+  }
+  .right-res{
+    margin-top:22px;
+  }
+}
+
+        </style>
+	<body>
+		
+		<!-- <nav class="top-nav">
+			<div class="nav-wrappper container">
+				<ul class="left">
+					<li><a href="index.php"><i class="material-symbols-outlined left">calendar_month</i>Schedule a free consultation</a></li>
+				</ul>
+				<ul class="right">
+					<li><a href="#!"><i class="material-symbols-outlined left">send</i><?php echo $email ?></a></li>
+					<li><a href="#!"><i class="material-symbols-outlined left">call</i><?php echo $phone ?></a></li>
+				</ul>
+			</div>
+		</nav> -->
+
+		<!-- <nav class="main-nav z-depth-0">
+			<div class="nav-wrapper container">
+				<a href="index.php/.." class="brand-logo"><?php echo $companyname ?></a>
+				<ul class="right">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="index.php">About Our Firm</a></li>
+					<li><a href="index.php">Our Services</a></li>
+					<li><a href="index.php">Plans & Pricing</a></li>
+					<li><a href="index.php">Contact Us</a></li>
+				</ul>
+			</div>
+		</nav> -->
+
+
+		<nav class="main-nav z-depth-0 main-nav-res">
+			<div class="nav-wrapper container-fluid">
+				<a href="#home" class="brand-logo"><?php echo $companyname ?></a>
+
+				<ul class="right right-res">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="index.php#industries">Our Expertise</a></li>
+					<li><a href="index.php#who-we-are">About Us</a></li>
+					<li id="contact-res"><a href="index.php#contact-us">Contact Us</a></li>
+					<li id="lets-res"><a href="index.php#contact-us" class="btn yellow black-text semi-bold"><i class="material-icons right">keyboard_arrow_right</i>Let's Talk</a></li>
+				</ul>
+			</div>
+		</nav>
+
+
+		<section>
+    <div class="container">
+        <h2 class="josefin center-align">Privacy Policy</h2>
+        <br><br>
+        <h5>YOUR PRIVACY IS A PRIORITY TO US</h5>
+        <p class="justify-text">At <?php echo $companyname ?>, safeguarding your privacy is of utmost importance to us. We assure you that <?php echo $companyname ?> will not disclose any nonpublic information about you to third parties beyond the <?php echo $companyname ?> corporate family without your explicit consent, except as outlined in our Privacy Policy.</p><br>
+
+<h5>PRIVACY POLICY</h5>
+<p class="justify-text">Your privacy matters greatly to <?php echo $companyname ?>. This Privacy Policy aims to clarify the information we collect, how we use it, and the measures we take to protect it. It also explains your rights concerning the use, access, and amendment of your personal data.</p><br>
+
+<h5>Information Collection Practices</h5>
+<p class="justify-text"><b>Website Visits</b></p><br>
+<p class="justify-text">When you browse our site, we do not collect any personal data simply by virtue of your visit. To enhance your experience, we automatically gather and store statistical data related to site traffic and usage from our server logs. This data may include the visitor's domain, IP address, browser type, operating system, internet service provider, duration of visit, pages accessed, and clickstream patterns. This aggregated information is anonymous and is used solely to improve your interaction with our website. We only attempt to identify users if there is a suspicion of unlawful activity.</p><br>
+
+<p class="justify-text"><b>Product Purchases</b></p><br>
+<p class="justify-text">Our website is designed for showcasing and selling our products, including gifts, home décor, and personalized items. Therefore, during the checkout process, you may be required to provide personal details such as your name, email address, shipping address, and phone number. Additionally, if you proceed with a purchase, we may request further information, including payment details and billing address.</p><br>
+
+<p class="justify-text"><b>Mobile Applications</b></p><br>
+<p class="justify-text">When you download and utilize our mobile application, we collect automatic data regarding your device type, performance metrics, and operating system. We implement mobile analytics tools to gain insights into the functionality of our app. This software may track metrics like usage frequency, events occurring within the app, aggregated performance data, and the app's download source. Importantly, we do not associate the information collected through our analytics with any personally identifiable information submitted via the app.</p><br>
+
+        
+<p class="justify-text">Occasionally, we may send you push notifications to keep you informed about upcoming events, promotions, or the launch of new products. If you prefer not to receive these notifications, you can disable them at the device settings level.</p><br>
+
+<p class="justify-text"><b>Tracking and Analytics Tools</b></p><br>
+<p class="justify-text"><?php echo $companyname ?> utilizes various technologies, including cookies, web beacons, tags, and scripts. Our site also employs web analytics services that monitor activities such as mouse clicks, movements, and scrolling. These tools help us analyze trends, manage the website, track user navigation, and collect demographic data about our user community. We use cookies to remember user preferences (like language) and for authentication purposes. You have the ability to manage cookie preferences through your individual browser settings. While rejecting cookies allows you to continue using our site, it may limit your access to certain features or sections.</p><br>
+
+<p class="justify-text"><b>Targeted Advertising</b></p><br>
+<p class="justify-text">We collaborate with a third-party service provider to display ads on our site and to manage our advertising on other platforms. This partner may use cookies to collect data regarding your interactions on our site and elsewhere, allowing them to deliver advertisements tailored to your browsing habits and interests. If you want to opt-out of having your data used for personalized advertising, you can do so by clicking here. However, please be aware that opting out does not eliminate ads; you may still see general advertisements.</p><br>
+
+<h5>How We Use Your Information</h5>
+<p class="justify-text">The information you provide is utilized to fulfill your orders and deliver the services you request. As permitted by law, we may also use your data to respond to inquiries submitted to our customer support, send promotional communications, or inform you about new products and services. We may use your information to present you with relevant offers and content, which may come from us or affiliated service providers.</p><br>
+
+<p class="justify-text">Additionally, we use the data we collect to monitor and investigate potential fraud, identity theft, and other unlawful activities while enhancing and personalizing our services.</p><br>
+
+<h5>Information Disclosure</h5>
+<p class="justify-text">Protecting your information is a top priority for us. We do not sell or rent your personal data, nor do we share it with third-party companies.</p><br>
+
+<p class="justify-text">In some cases, we may be legally obligated to disclose your personal information without your consent. This could include responding to lawful requests from public authorities, such as law enforcement or national security, or complying with subpoenas, court orders, or regulatory demands to prevent fraud.</p><br>
+
+<p class="justify-text">We may share your information with members of the <?php echo $companyname ?> Corporate family, all of whom adhere to our Privacy Policy.</p><br>
+
+<p class="justify-text">Your information may also be shared in relation to any proposed or completed sale, merger, or transfer of all or part of our business or relevant operational units.</p><br>
+
+        
+<h5>Refer a Friend</h5>
+<p class="justify-text">If you decide to use our referral feature to share our website with a friend, we will request their name and email address. An automatic electronic invitation will then be sent to your friend, encouraging them to visit our site. <?php echo $companyname ?> retains this information solely to facilitate the one-time email and to measure the effectiveness of our referral program. Your friend can reach out to us if they wish to have their information removed from our records.</p><br>
+
+<h5>Newsletters</h5>
+<p class="justify-text">We will utilize your name and email address to send you our newsletters. In respect for your privacy, we provide an option for you to unsubscribe at any time.</p><br>
+
+<h5>Customer Testimonials</h5>
+<p class="justify-text">We may display customer testimonials on our website that could include personal details such as the customer's name. We ensure that we have the customer's consent before publishing any testimonials. If you would like to update or remove your testimonial, please contact us at <?php echo $email ?>.</p><br>
+
+<h5>Community Blogs/Forums</h5>
+<p class="justify-text">Our website may feature publicly accessible blogs or community forums. Please be aware that any information shared in these areas may be viewed, collected, and utilized by others who have access to them. If you want your personal information removed from our blog or forum, please reach out to us at <?php echo $email ?>. In certain instances, we may not be able to remove your personal information, but we will inform you if that is the case and explain why.</p><br>
+
+<h5>Website Links</h5>
+<p class="justify-text">All links available on our website pertain to <?php echo $companyname ?>, including sections such as contact us, terms and conditions, privacy policy, and refund policy.</p><br>
+
+<h5>Security Measures and Information Integrity</h5>
+<p class="justify-text"><?php echo $companyname ?> is committed to safeguarding customer information. We comply with the security, regulatory, and privacy standards that apply to our industry. Our web applications are designed with proven security technologies, including firewalls, intrusion detection systems, encryption, access controls, and auditing practices. To protect your data as you enter it, <?php echo $companyname ?> employs SSL encryption provided by a trusted certificate authority.</p><br>
+
+        
+<h5>Information Retention</h5>
+<p class="justify-text">We will keep your information for as long as your account is active or as necessary to provide you with our services. If you wish to terminate your account or request that we stop using your information, please reach out to us at <?php echo $email ?>. We will retain and use your information as required to fulfill our legal obligations, resolve disputes, and enforce our agreements.</p><br>
+
+<h5>Your Rights</h5>
+<p class="justify-text">You have the right to request access to, correction of, or deletion of your personal information, as well as to receive a copy of the information we hold about you. To exercise any of these rights, please contact us at <?php echo $email ?>.</p><br>
+
+<h5>Policy Regarding Children</h5>
+<p class="justify-text">Our website is not designed for children under the age of 13. We do not knowingly collect personal information from children under 13. If you learn that a child under 13 has provided us with personal information, please inform us, and we will take appropriate measures to remove that information.</p><br>
+
+<h5>Amendments to This Privacy Policy</h5>
+<p class="justify-text">We may revise our Privacy Policy occasionally. Any changes will be communicated by posting the updated Privacy Policy on this page. We recommend that you review this Privacy Policy periodically for any updates. Changes to this Privacy Policy will take effect once they are posted on this page.</p><br>
+
+<h5>Get in Touch</h5>
+<p class="justify-text">If you have any inquiries regarding this Privacy Policy, please feel free to contact us:</p><br>
+<!-- <p class="justify-text">Via email: <?php echo $email ?><br>By phone: <?php echo $phone ?></p> -->
+
+        <br><br>
+    </div>
+</section>
+
+
+		<!-- <footer class="page-footer grey darken-4">
+			<div class="container">
+				<div class="row">
+					<div class="col l6 s12">
+						<h5 class="white-text">Who we are</h5>
+						<p class="grey-text text-lighten-4"><?php echo $companyname ?> is a team of numbers-obsessed, people-loving, bookkeepers and advisors devoted to the art of building personal connections through forward-thinking expertise, grounded in great communication and an entrepreneurial spirit. Clients tell us that we are approachable and that they feel taken care of after an interaction with one of our advisors.</p>
+					</div>
+					<div class="col l3 s12">
+						<h5 class="white-text">Policies</h5>
+						<p><a href="terms-and-conditions.php" class="white-text">Terms & Conditions</a></p>
+						<p><a href="privacy-policy.php" class="white-text">Privacy Policy</a></p>
+						<p><a href="refund-policy.php" class="white-text">Refund Policy</a></p>
+					</div>
+					<div class="col l3 s12">
+						<h5 class="white-text">Contact Information</h5>
+						<p><b><?php echo $companyname ?></b></p>
+						<p><b>Address : </b><?php echo $address ?></p>
+						<p><b>Toll Free : </b><?php echo $phone ?></p>
+						<p><b>Email : </b><?php echo $email ?></p>
+					</div>
+				</div>
+			</div>
+			<div class="footer-copyright indigo">
+				<div class="container">
+				&copy; <?php echo date('Y') ?> All Rights Reserved By <?php echo $companyname ?>
+				</div>
+			</div>
+		</footer> -->
+
+
+		<!--JavaScript at end of body for optimized loading-->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+		<script>
+
+
+			
+            function isMobileMode() {
+    return window.innerWidth <= 800;
+}
+
+			window.onscroll = () => {
+				// alert(1);
+                const navElement = document.querySelector('.main-nav');
+    const targetElement = document.querySelector('.right-res');
+
+    if (document.documentElement.scrollTop > 10) {
+        navElement.classList.remove('z-depth-0');
+
+        // Check if it's mobile mode and hide the element
+        if (isMobileMode()) {
+            targetElement.style.display = 'none';
+        }
+    } else {
+        navElement.classList.add('z-depth-0');
+
+        // Show the element again when z-depth-0 is added
+        targetElement.style.display = 'block';
+    }
+			}
+		</script>
+
+	</body>
+</html>
